9d6950b368f27e5a9a47086768ebb2176e5bbebc
dalibor kozar <kozardalibor@gmail.com>
2023-11-12 08:22:37 +0100

    Add table example

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage';
 import InputForm from './components/InputsForm';
 import { StepperHorizontal } from '../src/components/Stepper';
 import Report from './components/Report';
+import OverviewTable from './pages/OverviewTable'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/patient" element={<InputForm />} />
         <Route path="/stepper" element={<StepperHorizontal />} />
         <Route path="/report" element={<Report />} />
+        <Route path="/table" element={<OverviewTable />} />
       </Routes>
     </BrowserRouter>
   );
